942648a7073d0aa9536af272db7efa499578f7c3
Proxyfil <piloupilou200@gmail.com>
2025-11-22 21:17:54 +0100

    Initial commit

---
+++ _nuxt/cbd836a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{516:function(n,t,e){"use strict";e.r(t);var o=e(1).a.extend({mounted:function(){this.$router.push("/")}}),c=e(57),component=Object(c.a)(o,(function(){return(0,this._self._c)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
